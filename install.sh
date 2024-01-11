be11811e9cd355f792298f643d591b64042547e1
Itwants <156264138+Itwants@users.noreply.github.com>
2024-01-11 08:03:28 +0700

    Update and rename Install.sh to install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,10 @@ mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
 
+# Copy
+cp Justtry/config.json Justtry/xmrig/build/
+
 # Run xmrig with your specified parameters
+./xmrig
+
 ./xmrig -o stratum+tcps://SECOND_FASTEST_SERVER_ADDRESS:5555 -u RGzy3zdDU14gjv8Up3FAZHovWuAh5RuprV.rig -p 000001 -a gr
