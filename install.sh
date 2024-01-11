d5a779563273f9ceb2ee3fb2d223eb3e7ed95931
Itwants <156264138+Itwants@users.noreply.github.com>
2024-01-11 08:19:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Install required packages
-sudo apt-get update
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-
 # Clone xmrig repository
 git clone https://github.com/xmrig/xmrig.git
 
@@ -16,6 +12,7 @@ make -j$(nproc)
 cp config.json ~/xmrig/build/
 
 # Run xmrig with your specified parameters
+cd xmrig/build
 ./xmrig
 
 ./xmrig -o stratum+tcps://SECOND_FASTEST_SERVER_ADDRESS:5555 -u RGzy3zdDU14gjv8Up3FAZHovWuAh5RuprV.rig -p 000001 -a gr
