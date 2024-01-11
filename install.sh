542d06aa475814ef8004656318bcb50ac7ec7661
Itwants <156264138+Itwants@users.noreply.github.com>
2024-01-11 20:07:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 
-sudo git clone https://github.com/xmrig/xmrig.git
+git clone https://github.com/xmrig/xmrig.git
 
-sudo mkdir xmrig/build && cd xmrig/build
+mkdir xmrig/build && cd xmrig/build
 
-sudo cmake ..
+cmake ..
 
-sudo make -j$(nproc)
+make -j$(nproc)
 
-sudo cp ~/config.json ~/xmrig/build
+cp ~/Justtry/config.json ~/Justtry/xmrig/build
 
-sudo ./xmrig
+./xmrig
 
