72bac289c24c1c649e11ab4c5f7ddc12afdb5175
Itwants <156264138+Itwants@users.noreply.github.com>
2024-01-11 09:41:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Clone xmrig repository
 git clone https://github.com/xmrig/xmrig.git
 
-# Build xmrig
 mkdir xmrig/build && cd xmrig/build
+
 cmake ..
+
 make -j$(nproc)
 
-# Run xmrig with your specified parameters
-cd xmrig/build
+cp ~/config.json ~/xmrig/build
+
 ./xmrig
 
